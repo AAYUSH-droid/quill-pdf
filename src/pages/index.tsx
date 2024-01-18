e0195ee84d1a5b28770e050f4299eccc86ef5cb6
aayush <sharma31aayush@gmail.com>
2024-01-18 19:47:17 +0530

    add: navbar and fix: kinde-oss version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import { api } from "~/utils/api";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "~/components/ui/button";
 import Image from "next/image";
+import Navbar from "~/components/Navbar";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
+      <Navbar />
       <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
