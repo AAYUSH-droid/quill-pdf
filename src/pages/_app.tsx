e21da4e63872465023d2250beb57824da1bd3c85
aayush <sharma31aayush@gmail.com>
2024-01-18 15:31:34 +0530

    Add: landing page ui and tailwind setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
+import { Inter } from "next/font/google";
 import "~/styles/globals.css";
+import { cn } from "~/lib/utils";
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Component
+      {...pageProps}
+      className={cn(
+        "grainy min-h-screen font-sans antialiased",
+        inter.className,
+      )}
+    />
+  );
 };
 
 export default api.withTRPC(MyApp);
