60c62c522875588c55abf9784efc2f5c4f46af67
aayush <sharma31aayush@gmail.com>
2024-03-30 21:15:08 +0530

    Add x-trpc-source header to server context

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -14,6 +14,7 @@ const createContext = cache(() => {
   const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
 
+  // @ts-ignore
   return createTRPCContext({
     headers: heads,
   });
