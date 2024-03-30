96dac9cdad6c2b4b56759b95f2ec70e86adeb3d3
aayush <sharma31aayush@gmail.com>
2024-03-30 16:43:25 +0530

    add: dashboard page and upload file button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import Dashboard from "@/components/Dashboard";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Dashboard />
+    </div>
+  );
 };
 
 export default page;
--- src/server/api/routers/authCallback.ts
+++ src/server/api/routers/authCallback.ts
@@ -1,23 +1,19 @@
-import { z } from "zod";
-import { auth, currentUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { TRPCError } from "@trpc/server";
 import { db } from "@/server/db";
+import { TRPCError } from "@trpc/server";
 
 export const authCallbackRouter = createTRPCRouter({
   authCallback: publicProcedure.query(async () => {
     const { userId } = auth();
-
     if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
-
     //check if user is in the database
     const dbUser = await db.user.findFirst({
       where: {
         user_id: userId,
       },
     });
-
     if (!dbUser) {
       throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
     }
