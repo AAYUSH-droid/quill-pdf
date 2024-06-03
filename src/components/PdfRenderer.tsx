3dc7b61efd047ba717c0393c5636817da14c3076
aayush <sharma31aayush@gmail.com>
2024-06-03 15:31:08 +0530

    add: file renderer

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,11 +1,17 @@
+// import { Document, Page } from "react-pdf";
+// import "react-pdf/dist/Page/AnnotationLayer.css";
+// import "react-pdf/dist/Page/TextLayer.css";
+
 const PdfRenderer = () => {
   return (
     <div className="flex w-full flex-col items-center rounded-md bg-white shadow">
       <div className="flex h-14 w-full items-center justify-between border-b border-zinc-200 px-2">
         <div className="flex items-center gap-1.5">
-          <h1> pdf </h1>
+          <h1> pdf renderer </h1>
         </div>
       </div>
+
+      <div className="max-h-screen w-full flex-1"></div>
     </div>
   );
 };
